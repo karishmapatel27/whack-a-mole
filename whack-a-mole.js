5918588948392f6aaa594f80c8c631ee325eafa9
Karishma <karishmapatel93@hotmail.com>
2020-12-10 07:05:43 +1300

    chnage message

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -1,76 +1,58 @@
-//create variable and store cell reference in it.
-
+"use strict";
+exports.__esModule = true;
 var cellRef = document.getElementsByTagName("td");
 var len = cellRef.length;
-let score = 0;
-let timer = 0;
-let timeLimit = 10;
+var score = 0;
 document.getElementById("score").innerHTML = "Score: " + score;
-document.getElementById("start-btn").addEventListener("click", startGame)
-
-
-//create a function to select random cells
+document.getElementById("start-btn").addEventListener("click", startGame);
 function random() {
-  num = Math.floor(Math.random() * len);
-  console.log(num);
-  return num;
+    var num = Math.floor(Math.random() * len);
+    console.log(num);
+    return num;
 }
-
-//create a function to put mole into a cell.
 function display_mole() {
-  var mole = document.createElement("img");
-  mole.src = "./mole1.png";
-  mole.id = "mole";
-  mole.style.padding = "10px";
-  cellRef[random()].append(mole);
-  document.getElementById("mole").addEventListener("click", whackedMole);
+    var mole = document.createElement("img");
+    mole.src = "./images/mole1.png";
+    mole.id = "mole";
+    mole.style.padding = "10px";
+    cellRef[random()].append(mole);
+    document.getElementById("mole").addEventListener("click", whackedMole);
 }
-
 function whackedMole() {
-  playSound();
-  this.remove();
-  display_mole();
-  counter();
+    playSound();
+    this.remove();
+    display_mole();
+    counter();
 }
-
 function playSound() {
-  var sound = document.createElement("audio");
-  sound.src = "./whack-audio.wav";
-  sound.play();
-  return;
+    var sound = document.createElement("audio");
+    sound.src = "./whack-audio.wav";
+    sound.play();
+    return;
 }
-
-//count the score
 function counter() {
-  score++;
-  document.getElementById("score").innerHTML = "Score: " + score;
+    score++;
+    document.getElementById("score").innerHTML = "Score: " + score;
 }
-
 //set the timer 
 function countdown() {
-  var secs = 15;
-  function tick() {
-    var counter = document.getElementById("counter");
-    secs--;
-    counter.innerHTML = "0:" + (secs < 10 ? "0" : "") + String(secs);
-    if (secs >= 0) {
-      setTimeout(tick, 1000);
-    } else {
-      alert(`Game Over 
-      Your score is: ${score}`);
-      document.location.href = "";
+    var secs = 15;
+    function tick() {
+        var counter = document.getElementById("counter");
+        secs--;
+        counter.innerHTML = "0:" + (secs < 10 ? "0" : "") + String(secs);
+        if (secs >= 0) {
+            setTimeout(tick, 1000);
+        }
+        else {
+            alert("Game Over \n      You have whacked " + score + " moles");
+            document.location.href = "";
+        }
     }
-  }
-  tick();
+    tick();
 }
-
 //start the game 
 function startGame() {
-  display_mole();
-  countdown()
+    display_mole();
+    countdown();
 }
-
-
-
-
-
